bf3cee767f7f9416a49a925ed75e715226fe4c2a
jugrespan <julianablayk@gmail.com>
2025-01-20 00:04:57 -0300

    adicionei a função agregar amigos e atualizar a lista

--- app.js
+++ app.js
@@ -6,11 +6,30 @@ function adicionarAmigo() {
     let amigoNome =  document.querySelector("#amigo").value;
     if (amigoNome === "") {
         alert("Por favor insira um nome");
-        return 
+        return; 
     }
     amigos.push(amigoNome);
     document.querySelector("#amigo").value = "";
+    atualizarLista();
 }
 
+function atualizarLista() {
+    let listaAmigos = document.querySelector("#listaAmigos");
+    listaAmigos .innerHTML="";
+    for (let amigo of amigos) {
+        listaAmigos.innerHTML += "<li>" + amigo + "</li>";  
+    }
+}
 
-
+function sortearAmigo() {
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = "";
+    if (amigos.length === 0 )
+    {
+        alert("Por favor insira um amigo");
+        return;
+    }
+    let sorteado =Math.floor(Math.random() * amigos.length);
+    resultado.innerHTML = "<li>" + amigos[sorteado] + "</li>";
+    
+}
